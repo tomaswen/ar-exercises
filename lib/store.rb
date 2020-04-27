d48c8fb968e8110358180daec11d9e4aa9586e34
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-27 18:50:47 -0400

    add before create and destroy callbacks

--- lib/store.rb
+++ lib/store.rb
@@ -1,6 +1,17 @@
 class Store < ActiveRecord::Base
    has_many :employees
    validates :name, length: {minimum: 3}
-   validates :annual_revenue, numericality: {greater_than: 0}
+   validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}  
+   before_destroy :store_is_empty, prepend: true
+
+   private 
+   def store_is_empty
+      if employees.count > 0
+         errors.add(:base, "Can't be destroy because there are employees")
+         return false 
+      else 
+         return true
+      end
+   end
    
 end
